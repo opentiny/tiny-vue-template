a41b0a5df97e56714dc3fad8ca104db7fa519410
ajaxzheng <894103554@qq.com>
2025-04-18 17:02:12 +0800

    feat: 更新路由配置，使用 createWebHistory 替代 createMemoryHistory，并添加测试页面路由

---
+++ packages/tiny-vue-mobile-first/src/pages/test/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full h-screen max-h-full bg-black sm:bg-white">
+    <div class="flex flex-col max-h-full">22</div>
+  </div>
+</template>
--- packages/tiny-vue-mobile-first/src/router.ts
+++ packages/tiny-vue-mobile-first/src/router.ts
@@ -1,16 +1,18 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import Home from './pages/index/index.vue'
 import About from './pages/about/index.vue'
 import Detail from './pages/detail/index.vue'
+import Test from './pages/test/index.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
-  { path: '/detail', component: Detail }
+  { path: '/detail', component: Detail },
+  { path: '/test', component: Test }
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes
 })
