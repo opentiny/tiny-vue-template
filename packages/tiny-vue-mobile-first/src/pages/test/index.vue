bd9feccd1e80cb510605d398cdc8ac55ad4e82a8
ajaxzheng <894103554@qq.com>
2025-04-22 11:11:48 +0800

    chore: 优化测试页面的模板结构，移除多余的嵌套元素

--- packages/tiny-vue-mobile-first/src/pages/test/index.vue
+++ packages/tiny-vue-mobile-first/src/pages/test/index.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="w-full h-screen max-h-full bg-black sm:bg-white">
-    <div class="flex flex-col max-h-full">22</div>
-  </div>
+  <div class="w-full h-screen max-h-full bg-black sm:bg-white"></div>
 </template>
